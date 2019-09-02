a1b0a58e366e9a1a60eaa4f25e004312fc811940
mleichty <mleichty@iu.edu>
2019-09-02 11:27:04 -0400

    Updates

---
+++ precache-manifest.74b5e6e02a4bdcf72d38e14e0611ef96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18760fe8488806999072b896717c5e3a",
+    "url": "/react_cafe/index.html"
+  },
+  {
+    "revision": "45fa95a93f0c2f87d5ee",
+    "url": "/react_cafe/static/css/main.195b06c6.chunk.css"
+  },
+  {
+    "revision": "6000ca96638d5575b0d0",
+    "url": "/react_cafe/static/js/2.dc9fbbcd.chunk.js"
+  },
+  {
+    "revision": "45fa95a93f0c2f87d5ee",
+    "url": "/react_cafe/static/js/main.a925e63e.chunk.js"
+  },
+  {
+    "revision": "ae0acd035523c8566930",
+    "url": "/react_cafe/static/js/runtime~main.b4fd2650.js"
+  },
+  {
+    "revision": "31d8038ca9d6a7991b63b58ead9c78a6",
+    "url": "/react_cafe/static/media/barista.31d8038c.jpg"
+  },
+  {
+    "revision": "cdbc3b368c0fe618d329ca0f55d89bb2",
+    "url": "/react_cafe/static/media/drip.cdbc3b36.jpg"
+  },
+  {
+    "revision": "ebd8968b6082a82788692fe5bbc99632",
+    "url": "/react_cafe/static/media/espresso.ebd8968b.jpg"
+  },
+  {
+    "revision": "ab2f1dde31fe888a48708427dbe2f07e",
+    "url": "/react_cafe/static/media/french.ab2f1dde.jpg"
+  }
+]);
